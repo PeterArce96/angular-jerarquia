4b62b325b3cdefb450a5214e64826250aa73a238
PeterArce96 <parce0496@gmail.com>
2022-07-30 17:43:20 -0500

    componente en línea

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PadreComponent } from './padre/padre.component';
 import { HijoComponent } from './hijo/hijo.component';
+import { ComponenteEnLineaComponent } from './componente-en-linea/componente-en-linea.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PadreComponent,
-    HijoComponent
+    HijoComponent,
+    ComponenteEnLineaComponent
   ],
   imports: [
     BrowserModule
